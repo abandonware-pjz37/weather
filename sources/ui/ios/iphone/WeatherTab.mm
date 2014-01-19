7f86dda549c6add6a4924ac725d37541e63cdba7
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-01-19 16:01:22 +0400

    Add icon and description

--- sources/ui/ios/iphone/WeatherTab.mm
+++ sources/ui/ios/iphone/WeatherTab.mm
@@ -4,6 +4,8 @@
 #import "WeatherTab.h"
 
 #import <UIKit/UILabel.h>
+#import <UIKit/UIImageView.h>
+#import <UIKit/UIImage.h>
 #include "Weather.hpp"
 
 @interface WeatherTab ()
@@ -12,6 +14,8 @@
 @property (retain, nonatomic) IBOutlet UILabel *latitude;
 @property (retain, nonatomic) IBOutlet UILabel *temperature;
 @property (retain, nonatomic) IBOutlet UILabel *wind_speed;
+@property (retain, nonatomic) IBOutlet UILabel *description;
+@property (retain, nonatomic) IBOutlet UIImageView *icon;
 @end
 
 @implementation WeatherTab
@@ -21,11 +25,13 @@
   [super viewDidLoad];
 
   // hold some space, otherwise rectangle width becomes 0
-  NSString* placeholder = @"                ";
+  NSString* placeholder = @"                    ";
   [_latitude setText:placeholder];
   [_longitude setText:placeholder];
   [_temperature setText:placeholder];
   [_wind_speed setText:placeholder];
+  [_description setText:placeholder];
+  _icon.image = [UIImage imageNamed:@"empty"];
 
   // Do any additional setup after loading the view, typically from a nib.
 }
@@ -41,23 +47,10 @@
   const char* city = [textField.text UTF8String];
   if (!std::string(city).empty()) {
     Result result = Weather::get_result(city);
-    if (result.is_ok()) {
-      [_latitude setText:
-          [NSString stringWithUTF8String:result.lattitude().c_str()]
-      ];
-      [_longitude setText:
-          [NSString stringWithUTF8String:result.longitude().c_str()]
-      ];
-      [_temperature setText:
-          [NSString stringWithUTF8String:result.temperature().c_str()]
-      ];
-      [_wind_speed setText:
-          [NSString stringWithUTF8String:result.wind_speed().c_str()]
-      ];
-    }
-    else {
+    const Result::Opt<Result::Error>& error = result.error();
+    if (error.is_initialized()) {
       NSString* message = [
-          NSString stringWithUTF8String:result.error_message().c_str()
+          NSString stringWithUTF8String:error->error_message.c_str()
       ];
       UIAlertView* alert = [
           [UIAlertView alloc]
@@ -68,6 +61,36 @@
           otherButtonTitles:nil
       ];
       [alert show];
+    }
+    else {
+      const Result::Opt<Result::Mandatory>& mandatory = result.mandatory();
+      assert(mandatory.is_initialized());
+      [_latitude setText:
+          [NSString stringWithUTF8String:mandatory->lattitude.c_str()]
+      ];
+      [_longitude setText:
+          [NSString stringWithUTF8String:mandatory->longitude.c_str()]
+      ];
+      [_temperature setText:
+          [NSString stringWithUTF8String:mandatory->temperature.c_str()]
+      ];
+      [_wind_speed setText:
+          [NSString stringWithUTF8String:mandatory->wind_speed.c_str()]
+      ];
+      const Result::Opt<Result::Detailed>& detailed = result.detailed();
+      if (detailed.is_initialized()) {
+        [_description setText:
+            [NSString stringWithUTF8String:detailed->description.c_str()]
+        ];
+        NSString* image_name = [
+            NSString stringWithUTF8String:detailed->icon.c_str()
+        ];
+        _icon.image = [UIImage imageNamed:image_name];
+      }
+      else {
+        [_description setText:@""];
+        _icon.image = [UIImage imageNamed:@"empty"];
+      }
     }
   }
   [textField resignFirstResponder];
@@ -80,6 +103,8 @@
   [_temperature release];
   [_wind_speed release];
   [_city_field release];
+  [_description release];
+  [_icon release];
   [super dealloc];
 }
 
