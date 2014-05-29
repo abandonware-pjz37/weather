80a5d2cb04ed10b0631ae54510a6387666f0caf0
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-05-29 18:27:49 +0400

    Add progress indicator

---
+++ Source/weather/ui/ios/iphone/sugar.cmake
@@ -0,0 +1,16 @@
+# Copyright (c) 2014, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED WEATHER_UI_IOS_IPHONE_SUGAR_CMAKE_)
+  return()
+else()
+  set(WEATHER_UI_IOS_IPHONE_SUGAR_CMAKE_ 1)
+endif()
+
+include(sugar_files)
+
+sugar_files(
+    WEATHER_IOS_SOURCES
+    WeatherTab.h
+    WeatherTab.mm
+)
