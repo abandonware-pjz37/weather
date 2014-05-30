0dc272a48f4194fd3f47c7b2ea3aaa6d26ed75a3
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-05-30 20:45:58 +0400

    Add iPhone storyboard

---
+++ Source/weather/ui/ios/sugar.cmake
@@ -0,0 +1,33 @@
+# This file generated automatically by:
+#   generate_sugar_files.py
+# see wiki for more info:
+#   https://github.com/ruslo/sugar/wiki/Collecting-sources
+
+if(DEFINED WEATHER_UI_IOS_SUGAR_CMAKE_)
+  return()
+else()
+  set(WEATHER_UI_IOS_SUGAR_CMAKE_ 1)
+endif()
+
+include(sugar_files)
+include(sugar_include)
+
+sugar_include(iphone)
+sugar_include(resources)
+
+sugar_files(
+    WEATHER_IOS_SOURCES
+    Annotation.hpp
+    Annotation.mm
+    AppDelegate.hpp
+    AppDelegate.mm
+    EngineWrapper.hpp
+    EngineWrapper.mm
+    Progress.hpp
+    Progress.mm
+    SearchBar.hpp
+    SearchBar.mm
+    WeatherTabDelegate.hpp
+    WeatherTabDelegate.mm
+    main.mm
+)
