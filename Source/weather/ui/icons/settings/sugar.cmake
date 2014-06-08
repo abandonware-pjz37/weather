1919068aa09a096227f4a7dfee3f70d55a357ed7
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-06-08 12:28:32 +0400

    Rename iOS icons

--- Source/weather/ui/icons/settings/sugar.cmake
+++ Source/weather/ui/icons/settings/sugar.cmake
@@ -3,17 +3,16 @@
 # see wiki for more info:
 #   https://github.com/ruslo/sugar/wiki/Collecting-sources
 
-if(DEFINED WEATHER_UI_ICONS_APP_SUGAR_CMAKE_)
+if(DEFINED WEATHER_UI_ICONS_SETTINGS_SUGAR_CMAKE_)
   return()
 else()
-  set(WEATHER_UI_ICONS_APP_SUGAR_CMAKE_ 1)
+  set(WEATHER_UI_ICONS_SETTINGS_SUGAR_CMAKE_ 1)
 endif()
 
 include(sugar_files)
 
 sugar_files(
     WEATHER_IOS_RESOURCES
-    Icon_iPad_Non-Retina_76x76.png
-    Icon_iPad_Retina_152x152.png
-    Icon_iPhone_Retina_120x120.png
+    Icon_iPad_Non-Retina_29x29.png
+    Icon_iPhone_iPad_Retina_58x58.png
 )
