8dc7f639175926bc88dcae7d9c8b08f8528dfd81
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-06-20 15:59:15 +0400

    Update hunter gate

--- HunterGate.cmake
+++ HunterGate.cmake
@@ -14,8 +14,8 @@
 
 cmake_minimum_required(VERSION 2.8.10)
 
-set(HUNTER_MINIMUM_VERSION "0.3.0")
-set(HUNTER_MINIMUM_VERSION_HASH f9550a0bf6209d4da5fdee7736816b182d655f5f)
+set(HUNTER_MINIMUM_VERSION "0.3.4")
+set(HUNTER_MINIMUM_VERSION_HASH 6d3d74ee229b2c3c738a6ddb25677a611ba0beda)
 
 # Set HUNTER_ROOT cmake variable to suitable value.
 # Info about variable can be found in HUNTER_ROOT_INFO.
