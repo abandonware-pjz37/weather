dbb072b604d7a77abd3362de011a8c69d4c43c18
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-06-21 20:16:24 +0300

    Use cmake/Hunter/config.cmake to add custom versions

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,1 +1,4 @@
-hunter_config(Boost VERSION 1.57.0) # 1.58.0 issue https://svn.boost.org/trac/boost/ticket/11410
+# 1.58.0 issue https://svn.boost.org/trac/boost/ticket/11410
+# 1.57.0-1.56.0 Doesn't work on Linux (GCC, c++11)
+hunter_config(Boost VERSION 1.55.0-patched-3)
+hunter_config(Sober VERSION ${HUNTER_Sober_VERSION} CMAKE_ARGS SOBER_RELAX_WARNINGS=ON)
